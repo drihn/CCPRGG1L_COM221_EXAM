67cd76b94ff246ac9f755b029e9478327046840f
Aldrin A. Galvez <111488391+drihn@users.noreply.github.com>
2022-09-08 14:27:40 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+    
+    //Creates a new object name "Me" from student class
+    student me = new student("Galvez", "Aldrin", "A.", "06/01/04", 2022-104601, "galvezaa@students.national-u.edu.ph", true );
+    me.sayMyStudentNumber();
+    me.sayMyEmailAddress();
+    me.amIAwesome();
+    }
+    }
